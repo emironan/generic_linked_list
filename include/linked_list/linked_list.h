5c0d9000664576278166443aaec55af8769971f7
emironan <yusufonan96@gmail.com>
2025-01-14 18:00:52 +0300

    Added functionality to linked_list library for general purpose usage
    List of the functions added:
    
    1.  Print all list with given callback function                 DONE
    2.  GetFromHead                                                 DONE
    3.  GetFromTail                                                 DONE
    4.  Get with compare function                                   DONE
    5.  Get with index                                              DONE
    6.  Pop Tail                                                    DONE
    7.  Push Tail                                                   DONE
    8.  Pop Head                                                    DONE
    9.  Push Head                                                   DONE
    10. Return size of list                                         DONE
    11. Delete all list                                             DONE
    12. Remove with index                                           DONE
    13. Remove Head                                                 DONE
    14. Remove Tail                                                 DONE
    
    Tests are made in a basic main.c file but that is not added in
    this commit.
    A decent test structure will be included in the upcoming commits.

--- include/linked_list/linked_list.h
+++ include/linked_list/linked_list.h
@@ -2,7 +2,7 @@
  * File: linked_list.h
  * Author: Emir
  * Date: 10.01.2025
- * Description: generic linked list source file
+ * Description: generic linked list header file
  * Details:
  * Version: 1
  *****************************************************************************/
@@ -19,23 +19,27 @@ typedef enum e_linked_list_errors_t
     LL_ERROR_NONE = 0,
     LL_ERROR_ALLOCATION,
     LL_ERROR_INDEX_OUT_OF_BOUNDS,
+    LL_ERROR_LIST_ALREADY_EMPTY
 } linked_list_errors_t;
 
 /*****************************************************************************/
-/*
-    This is creating a new linked list
-*/
+/**
+ * @brief Creates a new linked list and returns the beginning address of it.
+ * @return [linked_list_t] Created linked list
+ */
 linked_list_t ll_create_linked_list(void);
 
 /*****************************************************************************/
-/*
-    Freeing the memory
-*/
+/**
+ * @brief Deletes all list iterating through all list
+ * @param[in] list [linked_list_t] the list to operator upon
+ * @return [void] 
+ */
 void ll_delete_linked_list(linked_list_t list);
 
 /*****************************************************************************/
 /**
- * @brief Adds the at the end of the list.
+ * @brief Adds data to the end of the list.
  * @param[in] list [linked_list_t] the list to operator upon
  * @param[in] data [void *] data to add
  * @param[in] data_size [size_t] size of the data
@@ -48,7 +52,7 @@ linked_list_errors_t ll_add_to_end(linked_list_t list,
 
 /*****************************************************************************/
 /**
- * @brief Adds the at the beginning of the list.
+ * @brief Adds data to the beginning of the list.
  * @param[in] list [linked_list_t] the list to operator upon
  * @param[in] data [void *] data to add
  * @param[in] data_size [size_t] size of the data
@@ -61,11 +65,51 @@ linked_list_errors_t ll_add_to_beginning(linked_list_t list,
 
 /*****************************************************************************/
 /**
- * @brief returns size of linked list
+ * @brief Pops data from end of the list
  * @param[in] list [linked_list_t] the list to operator upon
- * @return [int] returns the size of list
+ * @param[out] data [void *] the data pointer to return data
+ * @return [linked_list_errors_t] returns LL_ERROR_LIST_ALREADY_EMPTY if the
+ * list is already empty, otherwise returns LL_ERROR_NONE
  */
-int ll_return_list_size(linked_list_t list);
+linked_list_errors_t ll_pop_from_end(linked_list_t list, 
+                                     void* data,
+                                     size_t data_size);
+
+/*****************************************************************************/
+/**
+ * @brief Pops data from beginning of the list
+ * @param[in] list [linked_list_t] the list to operator upon
+ * @param[out] data [void *] the data pointer to return data
+ * @return [linked_list_errors_t] returns LL_ERROR_LIST_ALREADY_EMPTY if the
+ * list is already empty, otherwise returns LL_ERROR_NONE
+ */
+linked_list_errors_t ll_pop_from_beginning(linked_list_t list, 
+                                           void* data,
+                                           size_t data_size);
+
+/*****************************************************************************/
+/**
+ * @brief Gets data from end of the list
+ * @param[in] list [linked_list_t] the list to operator upon
+ * @param[out] data [void *] the data pointer to return data
+ * @return [linked_list_errors_t] returns LL_ERROR_LIST_ALREADY_EMPTY if the
+ * list is already empty, otherwise returns LL_ERROR_NONE
+ */
+linked_list_errors_t ll_get_from_end(linked_list_t list, 
+                                     void* data,
+                                     size_t data_size);
+
+/*****************************************************************************/
+/**
+ * @brief Gets data from beginning of the list
+ * @param[in] list [linked_list_t] the list to operator upon
+ * @param[out] data [void *] the data pointer to return data
+ * @return [linked_list_errors_t] returns LL_ERROR_LIST_ALREADY_EMPTY if the
+ * list is already empty, otherwise returns LL_ERROR_NONE
+ */
+linked_list_errors_t ll_get_from_beginning(linked_list_t list, 
+                                           void* data,
+                                           size_t data_size);
 
 /*****************************************************************************/
 /**
@@ -84,7 +128,54 @@ linked_list_errors_t ll_get_item_with_index(linked_list_t list,
                                             size_t data_size);
 
 /*****************************************************************************/
+/**
+ * @brief returns index of the data in the list, that is equal to given data 
+ * @param[in] list [linked_list_t] the list to operator upon
+ * @param[in] print_function [void (is_equal)(void* data1, void* data2))] 
+ * function that checks equality. if two data are same, it must return 0
+ * @return [int] index of found element in list
+ */
+int ll_get_index_of_eq_data(linked_list_t list,
+                            void* data,
+                            int (is_equal)(void* data1, void* data2));
+                                           
+/*****************************************************************************/
+/**
+ * @brief Removes data from end of the list
+ * @param[in] list [linked_list_t] the list to operator upon
+ * @return [linked_list_errors_t] returns LL_ERROR_LIST_ALREADY_EMPTY if the
+ * list is already empty, otherwise returns LL_ERROR_NONE
+ */
+linked_list_errors_t ll_remove_from_end(linked_list_t list);
 
+/*****************************************************************************/
+/**
+ * @brief Removes data from beginning of the list
+ * @param[in] list [linked_list_t] the list to operator upon
+ * @return [linked_list_errors_t] returns LL_ERROR_LIST_ALREADY_EMPTY if the
+ * list is already empty, otherwise returns LL_ERROR_NONE
+ */
+linked_list_errors_t ll_remove_from_beginning(linked_list_t list);
+
+/*****************************************************************************/
+/**
+ * @brief Removes data at the given index in given list
+ * @param[in] list [linked_list_t] the list to operator upon
+ * @param[in] index [int] the list to operator upon
+ * @return [linked_list_errors_t] returns LL_ERROR_INDEX_OUT_OF_BOUNDS if the
+ * given index is invalid for list, otherwise returns LL_ERROR_NONE
+ */
+linked_list_errors_t ll_remove_with_index(linked_list_t list, int index);
+
+/*****************************************************************************/
+/**
+ * @brief Returns size of linked list
+ * @param[in] list [linked_list_t] the list to operator upon
+ * @return [int] returns the size of list
+ */
+int ll_return_list_size(linked_list_t list);
+
+/*****************************************************************************/
 /**
  * @brief Prints all list using given print function from head to tail
  * @param[in] list [linked_list_t] the list to operator upon
--- src/linked_list/linked_list.c
+++ src/linked_list/linked_list.c
@@ -21,6 +21,25 @@ struct st_node_t
 };
 
 /*****************************************************************************/
+/**
+ * @brief Removes the given node by freeing the memory and makes the next
+ * pointer of it NULL. It must be used with end nodes.
+ * @param[in] node [node_t] the node to be removed
+ * @return [void]
+ */
+static void remove_end_node(node_t node);
+
+/*****************************************************************************/
+/**
+ * @brief iterates through list and returns the address of last node
+ * @param[in] list [linked_list_t] the list to operator upon
+ * @param[out] node [node_t] output to return address of last node
+ * @return [linked_list_errors_t] returns LL_ERROR_LIST_ALREADY_EMPTY if the
+ * list is already empty, otherwise returns LL_ERROR_NONE
+ */
+static linked_list_errors_t get_last_node(linked_list_t list, node_t* node);
+
+/*****************************************************************************/
 linked_list_t ll_create_linked_list(void)
 {
     linked_list_t linked_list = malloc(sizeof(struct st_node_t));
@@ -34,7 +53,6 @@ linked_list_t ll_create_linked_list(void)
 }
 
 /*****************************************************************************/
-/* Bütün listeyi dolaşıp silmesi gerekiyor. */
 void ll_delete_linked_list(linked_list_t list)
 {
     node_t node_holder = list;
@@ -109,16 +127,62 @@ linked_list_errors_t ll_add_to_beginning(linked_list_t list,
 }
 
 /*****************************************************************************/
-int ll_return_list_size(linked_list_t list)
+linked_list_errors_t ll_pop_from_end(linked_list_t list, 
+                                     void* data,
+                                     size_t data_size)
 {
-    int list_size = 0;
-    while(list->next != NULL)
+    if(ll_get_from_end(list, data, data_size) == LL_ERROR_LIST_ALREADY_EMPTY)
     {
-        list_size++;
-        list = list->next;
+        return LL_ERROR_LIST_ALREADY_EMPTY;
+    }
+    remove_end_node(list);
+
+    return LL_ERROR_NONE;
+}
+
+/*****************************************************************************/
+linked_list_errors_t ll_pop_from_beginning(linked_list_t list, 
+                                           void* data,
+                                           size_t data_size)
+{
+    if(ll_get_from_beginning(list, data, data_size) == LL_ERROR_LIST_ALREADY_EMPTY)
+    {
+        return LL_ERROR_LIST_ALREADY_EMPTY;
     }
 
-    return list_size;
+    ll_remove_from_beginning(list);
+
+    return LL_ERROR_NONE;
+}
+
+/*****************************************************************************/
+linked_list_errors_t ll_get_from_end(linked_list_t list, 
+                                     void* data,
+                                     size_t data_size)
+{
+    if(get_last_node(list, &list) == LL_ERROR_LIST_ALREADY_EMPTY)
+    {
+        return LL_ERROR_LIST_ALREADY_EMPTY;
+    }
+
+    memcpy(data, list->data, data_size);
+    
+    return LL_ERROR_NONE;
+}
+
+/*****************************************************************************/
+linked_list_errors_t ll_get_from_beginning(linked_list_t list, 
+                                           void* data,
+                                           size_t data_size)
+{
+    if(list->next == NULL)
+    {
+        return LL_ERROR_LIST_ALREADY_EMPTY;
+    }
+
+    memcpy(data, list->data, data_size);
+
+    return LL_ERROR_NONE;
 }
 
 /*****************************************************************************/
@@ -155,6 +219,122 @@ linked_list_errors_t ll_get_item_with_index(linked_list_t list,
 }
 
 /*****************************************************************************/
+int ll_get_index_of_eq_data(linked_list_t list,
+                            void* data,
+                            int (is_equal)(void* data1, void* data2))
+{
+    int index = 0;
+
+    // This means list is empty
+    if(list->next == NULL)
+    {
+        return -1;
+    }
+
+    while(list->next->next != NULL)
+    {
+        if(is_equal(list->data, data) == 0)
+        {
+            return index;
+        }
+
+        index++;
+        list = list->next;
+    }
+
+    /* -1 means couldn't found */
+    return -1;
+}
+
+/*****************************************************************************/
+linked_list_errors_t ll_remove_from_end(linked_list_t list)
+{
+    if(get_last_node(list, &list) == LL_ERROR_LIST_ALREADY_EMPTY)
+    {
+        return LL_ERROR_LIST_ALREADY_EMPTY;
+    }
+
+    remove_end_node(list);
+    return LL_ERROR_NONE;
+}
+
+/*****************************************************************************/
+linked_list_errors_t ll_remove_from_beginning(linked_list_t list)
+{
+    // will hold the next value of a free'd node
+    node_t node;
+
+    if(list->next == NULL)
+    {
+        return LL_ERROR_LIST_ALREADY_EMPTY;
+    }
+
+    free(list->data);
+    list->data = list->next->data;
+
+    node = list->next->next; 
+    free(list->next);
+    list->next = node;
+
+    return LL_ERROR_NONE;
+}
+
+/*****************************************************************************/
+linked_list_errors_t ll_remove_with_index(linked_list_t list, int index)
+{
+    // this is creted to hold value of the next node of the free'd one
+    node_t node;
+    
+    if(index < 0)
+    {
+        return LL_ERROR_INDEX_OUT_OF_BOUNDS;
+    }
+    // -1 is for stopping at the node just before our indexed node 
+    for(int i = 0; i < index - 1; i++)
+    {
+        list = list->next;
+        if(list == NULL)
+        {
+            return LL_ERROR_INDEX_OUT_OF_BOUNDS;
+        }
+    }
+    /* Always allocating the next node while filling the current. 
+    This is why we are controlling this here. */
+    if(list->next->next == NULL)
+    {
+        return LL_ERROR_INDEX_OUT_OF_BOUNDS;
+    }
+    
+    /* if it is the first index we can use the remove_from_beginning */
+    if(index != 0)
+    {
+        node = list->next;
+        list->next = list->next->next;
+        free(node->data);
+        free(node);
+    }
+    else
+    {
+        ll_remove_from_beginning(list);
+    }
+
+    return LL_ERROR_NONE;
+}
+
+/*****************************************************************************/
+int ll_return_list_size(linked_list_t list)
+{
+    int list_size = 0;
+    while(list->next != NULL)
+    {
+        list_size++;
+        list = list->next;
+    }
+
+    return list_size;
+}
+
+/*****************************************************************************/
 void ll_print_all_list(linked_list_t list, void (print_function)(void *))
 {
     print_function(list->data);
@@ -166,6 +346,32 @@ void ll_print_all_list(linked_list_t list, void (print_function)(void *))
             print_function(list->data);
         }
     }
+}
+
+/*****************************************************************************/
+static void remove_end_node(node_t node)
+{
+    free(node->next);
+    free(node->data);
+    node->next = NULL;
+}
+
+/*****************************************************************************/
+static linked_list_errors_t get_last_node(linked_list_t list, node_t* node)
+{
+    *node = list;
+
+    if(list->next == NULL)
+    {
+        return LL_ERROR_LIST_ALREADY_EMPTY;
+    }
+
+    while((*node)->next->next != NULL)
+    {
+        *node = (*node)->next;
+    }
+
+    return LL_ERROR_NONE;
 }
 
 /*****************************************************************************/
